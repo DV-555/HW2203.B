079ae1aec8f9cb251623fe4359bb33733950f38f
valen <valentin.dumler@gmail.com>
2023-03-23 09:17:22 +0100

    HW2203

---
+++ src/AgeComparator.java
@@ -0,0 +1,12 @@
+import java.util.Comparator;
+public class AgeComparator {
+
+        public static Comparator<Employee> ageComparator = new Comparator<Employee>() {
+            @Override
+            public int compare(Employee e1, Employee e2) {
+                return Integer.compare(e1.getAge(), e2.getAge());
+            }
+        };
+    }
+
+
